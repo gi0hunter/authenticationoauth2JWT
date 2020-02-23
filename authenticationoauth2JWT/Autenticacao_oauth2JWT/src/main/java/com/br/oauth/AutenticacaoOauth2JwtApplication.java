13b790190f1bebe935ddaac691b36c134efa913e
Giovani MC <37486962+gi0hunter@users.noreply.github.com>
2020-02-23 10:55:58 -0300

    projeto com oauth e jwt

---
+++ authenticationoauth2JWT/Autenticacao_oauth2JWT/src/main/java/com/br/oauth/AutenticacaoOauth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.br.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoOauth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoOauth2JwtApplication.class, args);
+	}
+
+}
---
+++ authenticationoauth2JWT/Autenticacao_oauth2JWT/src/test/java/com/br/oauth/AutenticacaoOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoOauth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
